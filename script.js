d21d0e3683b04b0a84fec6b31415d0834812f27a
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-15 02:18:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const extendHex = (shortHex) => {
   // write your code here
-	let result ="#";
-	for(let i=0; i<shortHex.length; i++){
+	if(shortHex.charAt(0)=="#"){
+		let result = "#";
+	for(let i=1; i<shortHex.length; i++){
 		result = result + shortHex.charAt(i) + shortHex.charAt(i);
 	}
-	return result;
+		return result;
+	}
+	else if(shortHex.charAt(0)!="#"){
+		let result2 = "#";
+		for(let i=0; i<shortHex.length; i++){
+		result2 = result2 + shortHex.charAt(i) + shortHex.charAt(i);
+	}
+		return result2;
+	}
+	else if('A'<=shortHex.charAt(0)<='Z'){
+		shortHex.toUpperCase();
+		let result3 = "#";
+		for(let i=0; i<shortHex.length; i++){
+		result3 = result3 + shortHex.charAt(i) + shortHex.charAt(i);
+	}
+		return result3;
+	}
 };
 
-// Do not change the code below.hh
- const shortHex = prompt("Enter Short Hex.");
- alert(extendHex(shortHex));
+// Do not change the code below.hhg
+   // const shortHex = prompt("Enter Short Hex.");
+   // alert(extendHex(shortHex));
