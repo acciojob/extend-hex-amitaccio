367d707533f9ae21a7852f337638194070f576cd
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-15 01:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let result ="#";
+	for(let i=0; i<shortHex.length; i++){
+		result = result + shortHex.charAt(i) + shortHex.charAt(i);
+	}
+	return result;
 };
 
-// Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// Do not change the code below.hh
+ const shortHex = prompt("Enter Short Hex.");
+ alert(extendHex(shortHex));
